105b69ba0ce17e39aa2c187199102bcdffd578e1
Nicolas IRAGNE <niragne@e3r6p12.42.fr>
2019-04-28 15:37:09 +0200

    testg

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+	public int Damage;
+	public float Range;
+	public int Ammo;
+	public float ReloadTime;
+	public Sprite WorldModel;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
